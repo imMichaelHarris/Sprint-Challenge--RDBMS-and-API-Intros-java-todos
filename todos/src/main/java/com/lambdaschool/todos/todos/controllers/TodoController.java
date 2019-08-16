4584b87293ae352e888540a75c6f497ed3bfe9da
Michael Harris <mhguitar1997@gmail.com>
2019-08-16 09:47:15 -0700

    Setting todo controller up

---
+++ todos/src/main/java/com/lambdaschool/todos/todos/controllers/TodoController.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.todos.todos.controllers;
+
+import com.lambdaschool.todos.todos.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PreAuthorize("hasRole('user') or hasRole('admin')")
+@RequestMapping("/user")
+public class TodoController {
+    @Autowired
+    TodoService todoService;
+}
--- todos/src/main/java/com/lambdaschool/todos/todos/repos/ToDoRepository.java
+++ todos/src/main/java/com/lambdaschool/todos/todos/repos/ToDoRepository.java
@@ -9,4 +9,9 @@ import java.util.List;
 public interface ToDoRepository extends CrudRepository<Todo, Long> {
 //    @Query(value = )
 //    List<>
+
+//    SELECT u.username as user, u.password as pass, u.USERID as userid, t.description as desc, t.completed as completed, t.todoid as todoid
+//    FROM USERS u JOIN TODOS t
+//    ON u.userid = t.userid
+//    GROUP BY t.description
 }
